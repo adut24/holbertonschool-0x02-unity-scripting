7ea96c4a8854cdd3bd8714f6f856ddab57ae719a
adut24 <dutertre.alexandre@laposte.net>
2023-04-25 10:13:01 +0200

    Add Unity files

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+/// <summary>
+/// Represents the control of the player
+/// </summary>
+public class PlayerController : MonoBehaviour
+{
+	public float speed;
+
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	void FixedUpdate()
+	{
+
+	}
+}
